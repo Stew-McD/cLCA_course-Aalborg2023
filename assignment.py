d1c564fba24f50d574c043e0f62ef5cf6f0b4a3d
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-05-04 15:40:05 +0200

    added inventory xlsx

---
+++ assignment.py
@@ -0,0 +1,18 @@
+
+
+#%%
+import bw2data as bd
+
+bd.__version__
+
+# %%
+
+bd.projects.set_current("cLCA-aalborg")
+
+db = bd.Database("con391")
+
+# %%
+
+act_list_ei = 
+
+db.search("")
---
+++ import.py
@@ -0,0 +1,28 @@
+
+#%%
+
+import bw2io as bi
+import bw2data as bd
+
+bi.__version__
+
+# %%
+
+bd.projects.set_current("cLCA-aalborg")
+
+bi.bw2setup()
+
+db_name = "con391"
+con_path = "/home/stew/CML/coding/DBs/EI/con391/datasets"
+
+ei = bi.SingleOutputEcospold2Importer(con_path, db_name)
+
+
+ei.apply_strategies()
+ei.statistics()
+ei.write_database()
+db = bd.Database(db_name)
+db.metadata
+
+# %%
+
