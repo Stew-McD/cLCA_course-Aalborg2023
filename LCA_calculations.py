c68de70fed885e83ce85df0b3828832ff5891c62
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-05-16 19:18:24 +0200

    ""

--- LCA_calculations.py
+++ LCA_calculations.py
@@ -9,67 +9,75 @@ import bw2calc as bc
 #%% load the project and define the databases
 
 #%% 
-# list all the projects
-list(bd.projects)
+# # list all the projects
+# list(bd.projects)
 
-# open the project in brightway2
-bd.projects.set_current('cLCA-aalborg')
+# # open the project in brightway2
+# bd.projects.set_current('cLCA-aalborg')
 
-# list the databases in the project
-list(bd.databases)
+# # list the databases in the project
+# list(bd.databases)
 
-#  Define the databases
-ei = bd.Database('con391')
-bio = bd.Database('biosphere3')
-fg = bd.Database('fg_csv')
+# #  Define the databases
+# ei = bd.Database('con391')
+# bio = bd.Database('biosphere3')
+# fg = bd.Database('fg_csv')
 
-# look for the activity in the database
-fg.search('Succinic')
+# # look for the activity in the database
+# fg.search('Succinic')
 
-#%% Set the functional unit for the LCA calculation
+db_name = 'corn'
+def get_LCA_scores(model):    #%% Set the functional unit for the LCA calculation
+    db_name = "fg_"+model
+    fg = bd.Database(db_name)
+    fu = {'name' : 'Succinic acid production ({})'.format(model), 'amount': 1, 'unit': 'kg'}
+    # find it in the database
+    myact = fg.get(fu['name'])
+    # another way to do it, looking at all databases in the project
+    # myact2 = bd.get_node(name=fu['name'])
+    # check that they are the same
+    # myact2 == myact
 
-fu = {'name' : 'Succinic acid production', 'amount': 1, 'unit': 'kg'}
-# find it in the database
-myact = fg.get(fu['name'])
-# another way to do it, looking at all databases in the project
-myact2 = bd.get_node(name=fu['name'])
-# check that they are the same
-myact2 == myact
-
-# look at the exchanges of the functional unit
-print("The functional unit is: \n{}, with an amount of {} {}".format(myact['name'], fu['amount'], myact['unit']))
-exchanges = list(myact.exchanges())
-print("*****\nThe exchanges of the functional unit are: \n")
-print(*exchanges, sep = "\n\n")
+    # look at the exchanges of the functional unit
+    # print("The functional unit is: \n{}, with an amount of {} {}".format(myact['name'], fu['amount'], myact['unit']))
+    # exchanges = list(myact.exchanges())
+    # print("*****\nThe exchanges of the functional unit are: \n")
+    # print(*exchanges, sep = "\n\n")
 
 
-#%% Define the method for the LCA calculation
-# list all the methods available
-method_list = list(bd.methods)
+#Define the method for the LCA calculation
+    # list all the methods available
+    # method_list = list(bd.methods)
 
-'''
-eg. 
-('CML v4.8 2016 no LT',
- 'acidification no LT',
- 'acidification (incl. fate, average Europe total, A&B) no LT')
-'''
 
-# choose one
-method_search_string = 'material'
-method_search = [x for x in method_list if method_search_string in " ".join(x).lower()]
-#print(method_search)
+# eg. 
+# ('CML v4.8 2016 no LT',
+# 'acidification no LT',
+# 'acidification (incl. fate, average Europe total, A&B) no LT')
 
-# you can also do it like this
-mymethod = ('IPCC 2013', 'climate change', 'global warming potential (GWP100)')
-mymethod_object = bd.Method(mymethod)
 
-#%% Run the LCA calculation
-# set to FU for the 
-functional_unit = {bd.get_node(name=fu['name']): fu['amount']}
-lca = bc.LCA(functional_unit, mymethod)
-lca.lci()
-lca.lcia()
+    # choose one
+    # method_search_string = 'material'
+    # method_search = [x for x in method_list if method_search_string in " ".join(x).lower()]
+    #print(method_search)
 
-#%% print the results
-print("*****************\n\t For the FU: '{}' \n\t with the method '{}' \n\t, the LCIA score is: {}".format(functional_unit, lca.method, round(lca.score, 2)), bd.Method(mymethod).metadata['unit'])
-# %%
+    # you can also do it like this
+    mymethod = ('IPCC 2013', 'climate change', 'global warming potential (GWP100)')
+    # mymethod_object = bd.Method(mymethod)
+
+    #%% Run the LCA calculation
+    # set to FU for the 
+    functional_unit = {bd.get_node(name=fu['name']): fu['amount']}
+    lca = bc.LCA(functional_unit, mymethod)
+    lca.lci()
+    lca.lcia()
+
+    #%% print the results
+    print("*****************\n\t For the FU: '{}' \n\t with the method '{}' \n\t, the LCIA score is: {}".format(functional_unit, lca.method, round(lca.score, 2)), bd.Method(mymethod).metadata['unit'])
+
+# write results to a file
+
+    with open('LCA_results.txt', 'a') as f:
+        f.write("*****************\n\t For the FU: '{}' \n\t with the method '{}' \n\t, the LCIA score is: {}".format(functional_unit, lca.method, round(lca.score, 2)), bd.Method(mymethod).metadata['unit'])
+
+    # %%
--- import_csv.py
+++ import_csv.py
@@ -6,7 +6,7 @@ import bw2data as bd
 import bw2calc as bc
 
 # %% Define custom fuction to covert the dataframe from the csv into a set of dictionaries ready for the bw2io to make a database
-# from Massimo 'lci_to_bw2.py'
+#%% from Massimo 'lci_to_bw2.py'
 
 def lci_to_bw2(db_df):
     '''A function to convert a pd.Dataframe to a dict
@@ -61,67 +61,43 @@ def lci_to_bw2(db_df):
      
     bw2_db = dict(zip(db_keys, db_values))
     
-    return bw2_db # We have a dict to be used as database. Perfect.
-# %% Import the csv file and convert it to a database
-db_df = pd.read_excel('data/Inventory_data_brightway2_corn.xlsx', header = 0)
+    return bw2_db
 
-# db_df = pd.read_csv('data/Inventory_data_brightway2.csv', header = 0, sep = ";")
-db_df.head()
+def write_database(model):
+    # apply the function to the dataframe to get a dictionary ready for bw2io
+    db_name = 'fg_'+model
+    db_df = pd.read_csv('data/{}.csv'.format(db_name), sep = ';')
 
-#%%
-# change names of databases to match  how they are in my project
-db_df.replace("exldb", "fg_corn", inplace=True)
-db_df.replace("HCL", "HCl", inplace=True) # fix spelling error
-db_df.replace("ecoinvent 3.9 conseq", "con391", inplace=True)
+    db = lci_to_bw2(db_df)
 
-#%% Define the databases
-ei = bd.Database('con391')
-bio = bd.Database('biosphere3')
-
-# apply the function to the dataframe to get a dictionary ready for bw2io
-db = lci_to_bw2(db_df)
-
-# open the project in brightway2
-bd.projects.set_current('cLCA-aalborg')
-
-# list the databases in the project
-bd.databases
-
-# delete the database if it already exists
-try: 
-    del bd.databases['fg_corn']
-except:
-    pass
+    # delete the database if it already exists
+    try: 
+        del bd.databases[db_name]
+    except:
+        pass
 
 # write the database to the project
-fg = bd.Database('fg_corn')
-fg.write(db)
+    fg = bd.Database(db_name)
+    fg.write(db)
+    
+    #print("*******\n", fg.name, "\n", fg.metadata)
 
 # check that it worked
-bd.databases
-fg.metadata
-fg_dict = fg.load()
-print(*fg_dict, sep = "\n")
+    bd.databases
+    fg.metadata
+    fg_dict = fg.load()
+    print(*fg_dict, sep = "\n")
 
-#%%  Inspect the database
-for act in fg:
-    dict = act.as_dict()
-    print("\n")
-    print("*****************************")
-    print("{} : {} : {}".format(dict['name'], dict["unit"], dict["code"]))
-    print("----------------------------")
-    print("TECHNOSPHERE EXCHANGES")
-    print(*list(act.technosphere()))
-    print("----------------------------")
-    print("BIOSPHERE EXCHANGES")
-    print(*list(act.biosphere()))
-    print("*****************************")
-
-
-#%% Another way to inspect the database, using list comprehensions
-
-[[print(act, exc) for exc in list(act.technosphere())]for act in fg]  # check more stuff 
-print('---------')
-[[print(exc.uncertainty) for exc in list(act.biosphere())]for act in fg]  # check more stuff
-
-# now go to LCA_calculation.py
+# #%%  Inspect the database
+# for act in fg:
+#     dict = act.as_dict()
+#     print("\n")
+#     print("*****************************")
+#     print("{} : {} : {}".format(dict['name'], dict["unit"], dict["code"]))
+#     print("----------------------------")
+#     print("TECHNOSPHERE EXCHANGES")
+#     print(*list(act.technosphere()))
+#     print("----------------------------")
+#     print("BIOSPHERE EXCHANGES")
+#     print(*list(act.biosphere()))
+#     print("*****************************")
